d574b5d4d1320cfdea9aeca1db8868f2aa018d87
elfirl <mike.bilter@gmail.com>
2015-11-14 18:12:34 -0500

    Working on the logic for grabbing the data

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     
+	$(".input-submit-button").on("click", function() {
+			var fields = {};
+			$("form [name]").each(function() {
+				fields[this.name] = this.value;
+			});
+			console.log(fields);
+	});
+
+	
 
 
 });
