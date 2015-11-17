04eab00234df3d73052ce7223de8abea1c951f22
elfirl <mike.bilter@gmail.com>
2015-11-16 21:17:35 -0500

    Changed my object to an array, rewrote the JS, so far so good.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,49 @@
 $(document).ready(function() {
 
-
-	// Checks to see if the on click event is triggerd from the button with class input-submit-button.
+	// Executs funcitons when an element with the specified class is clicked.
 	$(".input-submit-button").on("click", function(event) {
-		// Prevents the default behavior from the button submitting.
 		event.preventDefault();
 
-		// Make a variable and assigns an empty object into it.
-		var rolls = {};
-
-		// Grabs anything with a class of combatant, and for each one, executes a function with parameters index and field
-		$(".combatant").each(function(index, field) {
-			// jQuery looks for any field with a class of name, gets its input value, then assigns that to the variable.
-			var name = $(field).find(".name").val();
-			// jQuery looks for any field with a class of roll, gets its input value, then assigns that to the variable.
-			var roll = $(field).find(".roll").val();
-			// Adds the object pair of {name: roll} based on the above variables. How does it append vs overwrite?
-			rolls[name] = roll;
-		});
-
-		// Grabs each key pair from rolls, and adds the pairs to <li>, to a <ul> with id of list.
-		$.each(rolls, function(key, value){
-			$("#list").append("<li>" + key + " " + value + "</li>");
-		});
-
-		console.log(rolls);
+		// The order here is important - create the array, sort by roll, display the sorted list on screen.
+		var rolls = getCombatants();
+		rolls.sort(sortArraysByRoll);
+		displayRolls(rolls);
 	});
+
+	// Sets the value of name and roll to the input field specified, and puts them in an array of arrays.
+	function getCombatants() {
+		var rolls = [];
+
+		$(".combatant").each(function(index, field) {
+			var name = $(field).find(".name").val();
+			var roll = $(field).find(".roll").val();
+
+			// If both vars have a value, make a new array containing both inside of the rolls array.
+			if(name && roll) {
+				rolls.push([name, roll]);
+			}
+		});
+
+		return rolls;
+	}
+
+	// Takes the rolls, and displays them in a list of <li>.
+	function displayRolls(rolls) {
+		$.each(rolls, function(key, value){
+			var name = value[0];
+			var roll = value[1];
+
+			$("#list").append("<li>" + name + " " + roll + "</li>");
+		});
+	}
+
+	// Sorts the output in decending order. a - b for ascending.
+	function sortArraysByRoll(a, b) {
+		return b[1] - a[1];
+	}
+
+
+
+
 
 });
